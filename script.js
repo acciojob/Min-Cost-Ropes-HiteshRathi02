fe4ec54238590a068e66700ff2ff925805ddc8e5
Hitesh Rathi <97667730+HiteshRathi02@users.noreply.github.com>
2025-07-07 14:53:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+
+function mincost(arr) {
+  let totalCost = 0;
+
+  while (arr.length > 1) {
+    // Sort the array to get two smallest ropes
+    arr.sort((a, b) => a - b);
+
+    let first = arr.shift(); // smallest
+    let second = arr.shift(); // second smallest
+
+    let cost = first + second;
+    totalCost += cost;
+
+    // Push the combined rope back into the array
+    arr.push(cost);
+  }
+
+  return totalCost;
 }
 
 module.exports=mincost;
